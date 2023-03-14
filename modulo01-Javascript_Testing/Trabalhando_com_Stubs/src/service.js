14cb0e5d0ce424b1f309eda4ac9531cba80b9535
GugaSantos <guga.santos.guga@gmail.com>
2023-03-14 18:59:07 -0300

    test: service

--- modulo01-Javascript_Testing/Trabalhando_com_Stubs/src/service.js
+++ modulo01-Javascript_Testing/Trabalhando_com_Stubs/src/service.js
@@ -4,7 +4,12 @@ class Service {
   }
 
   async getPlanets(url) {
-
+    const data = await this.makeRequest(url);
+    return {
+      name: data.name ,
+      surfaceWater: data['surface_water'],
+      appeardIn: data.films.length
+    }
   }
 }
 
--- modulo01-Javascript_Testing/Trabalhando_com_Stubs/src/service.test.js
+++ modulo01-Javascript_Testing/Trabalhando_com_Stubs/src/service.test.js
@@ -2,12 +2,55 @@ const Service = require('./service');
 const assert = require('assert');
 const BASE_URL_1 = "https://swapi.dev/api/planets/1/";
 const BASE_URL_2 = "https://swapi.dev/api/planets/2/";
+const { createSandbox } = require('sinon');
+const sinon = createSandbox();
+const mocks = {
+  alderaan: require('./../mocks/alderaan.json'),
+  tatooine: require('./../mocks/tatooine.json')
+}
 
 //IFEE
 ;(async () => {
+  // {
+  //   // Vai para a internet!!
+  //   const service = new Service();
+  //   const data = await service.makeRequest(BASE_URL_2)
+  //   console.log(JSON.stringify(data));
+  // }
+    
+  const service = new Service();
+  const stub = sinon.stub(
+    service,
+    service.makeRequest.name
+  )
+
+  stub
+  .withArgs(BASE_URL_1)
+  .resolves(mocks.tatooine)
+
+  stub
+  .withArgs(BASE_URL_2)
+  .resolves(mocks.alderaan)
+
   {
-    const service = new Service();
-    const data = await service.makeRequest(BASE_URL_2)
-    console.log(JSON.stringify(data));
+    const expected = {
+      name: "Tatooine",
+      surfaceWater: "1",
+      appeardIn: 5 
+    }
+
+    const results = await service.getPlanets(BASE_URL_1)
+    assert.deepStrictEqual(results, expected)
+  }
+
+  {
+    const expected = {
+      name: "Alderaan",
+      surfaceWater: "40",
+      appeardIn: 2
+    }
+
+    const results = await service.getPlanets(BASE_URL_2)
+    assert.deepStrictEqual(results, expected)
   }
 })();
