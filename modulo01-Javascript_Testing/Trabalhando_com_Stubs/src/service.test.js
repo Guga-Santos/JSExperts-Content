2c06d31216e90c6106240d28bd841bdc5f2aeda4
GugaSantos <guga.santos.guga@gmail.com>
2023-03-14 18:38:51 -0300

    feat: create mocks

--- modulo01-Javascript_Testing/Trabalhando_com_Stubs/src/service.test.js
+++ modulo01-Javascript_Testing/Trabalhando_com_Stubs/src/service.test.js
@@ -7,7 +7,7 @@ const BASE_URL_2 = "https://swapi.dev/api/planets/2/";
 ;(async () => {
   {
     const service = new Service();
-    const data =  service.makeRequest(BASE_URL_1)
-    console.log(data);
+    const data = await service.makeRequest(BASE_URL_2)
+    console.log(JSON.stringify(data));
   }
 })();
