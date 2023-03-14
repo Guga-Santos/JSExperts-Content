e8b8118e6fe8fe50eb35bfd980f1634d7355544f
GugaSantos <guga.santos.guga@gmail.com>
2023-03-14 01:45:58 -0300

    feat: create isValid function

--- modulo01-Javascript_Testing/Trabalhando_com_Mocks/src/file.js
+++ modulo01-Javascript_Testing/Trabalhando_com_Mocks/src/file.js
@@ -1,6 +1,25 @@
+const { readFile } = require('fs/promises'); 
+const { error } = require('./constants');
+const DEFAULT_OPTION = {
+  maxLines: 3,
+  fields: ['id', 'name', 'profession', 'age']
+}
+
 class File {
   static async csvToJSON(filePath) {
-
+    const content = await readFile(filePath, 'utf8');
+    const validation = this.isValid(content);
+    if(!validation.valid) throw new Error(validation.error);
+  }
+  static isValid(csvString, options = DEFAULT_OPTION) {
+    const [headers, ...fileWithoutHeader] = csvString.split(/\r?\n/)
+    if(!fileWithoutHeader.length) {
+      return {
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false
+      }
+    }
+    console.log(headers, fileWithoutHeader)
   }
 }
 
