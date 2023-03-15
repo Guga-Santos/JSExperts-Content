71df71ca19d644c7d6239fa5ac4fa57e0f76aba6
GugaSantos <guga.santos.guga@gmail.com>
2023-03-15 16:42:56 -0300

    feat: create routes

--- modulo01-Javascript_Testing/04-Trabalhando_com_E2E_e_Coverage/src/api.js
+++ modulo01-Javascript_Testing/04-Trabalhando_com_E2E_e_Coverage/src/api.js
@@ -1,8 +1,32 @@
 const http = require('http');
+const { once } = require('events');
+const DEFAULT_USER = {
+  username: 'GustavoSantos',
+  password: '123456'
+}
+
+const routes = {
+  '/contact:get': (request, response) => { 
+    response.write('Contact ours page')
+    response.end() },
+
+  '/login:post': async (request, response) => { 
+    const data = await JSON.parse(once(request, 'data'));
+    console.log('data', data)
+    response.end() },
+
+  default(request, response) {
+    response.writeHead(404) 
+    return response.end('Not Found!')
+  }
+}
 
 function handler(request, response) {
-  return response.end('ok')
+  const { url, method } = request;
+  const routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`;
+  const chosen = routes[routeKey] || routes.default
+  return chosen(request, response);
 }
 
 const app = http.createServer(handler)
-.listen(3000, () => console.log('Rodando na porta 3000'));
+.listen(3000, () => console.log('Running at 3000'))
