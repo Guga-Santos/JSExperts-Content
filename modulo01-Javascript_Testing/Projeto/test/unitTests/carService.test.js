9ef8b5a485a00ff97dbb280a25071b20a9ed8215
GugaSantos <guga.santos.guga@gmail.com>
2023-03-16 22:57:41 -0300

    feat: create test case

--- modulo01-Javascript_Testing/Projeto/test/unitTests/carService.test.js
+++ modulo01-Javascript_Testing/Projeto/test/unitTests/carService.test.js
@@ -1,22 +1,32 @@
 const { describe, it, before, after } = require('mocha');
-const CarService = require('./../../src/service/carService')
+const CarService = require('./../../src/service/carService');
 
-const { join } = require('path')
-const assert = require('assert')
+const { join } = require('path');
+const assert = require('assert');
 
 const carsDatabase = join(__dirname, './../../database', 'cars.json');
 
+const mocks = {
+  validCarCategory: require('./../mocks/valid-carCategory.json'),
+  validCar: require('./../mocks/valid-car.json'),
+  validCustomer: require('./../mocks/valid-customer.json')
+}
+
 describe('CarService Suite Tests', () => {
-  let carService = {}
+  let carService = {};
   before(() => {
     carService = new CarService({
       cars: carsDatabase
     });
   })
   it('Ensure its possible return a avaiable car', async () => {
-    const result = await carService.getAvailableCar()
-    const expected = null
+    const car = mocks.validCar;
+    const carCategory = Object.create(mocks.validCarCategory);
+    carCategory.ids = [car.id];
+
+    const result = await carService.getAvailableCar(carCategory);
+    const expected = car;
     
-    assert.deepStrictEqual(result, expected)
+    assert.deepStrictEqual(result, expected);
   })
 })
