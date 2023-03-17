2b1e5749c09beea5e29c54ad824fe7daa86c36c0
GugaSantos <guga.santos.guga@gmail.com>
2023-03-17 03:25:20 -0300

    feat: create calculateFinalPrice

--- modulo01-Javascript_Testing/Projeto/src/service/carService.js
+++ modulo01-Javascript_Testing/Projeto/src/service/carService.js
@@ -1,14 +1,28 @@
 const BaseRepository = require("../repository/base/baseRepository");
+const Tax = require('./../entities/tax');
 
 class CarService {
   constructor({ cars }) {
     this.carRepository = new BaseRepository({ file: cars })
-
+    this.taxesBasedOnAge = Tax.taxesBasedOnAge;
     this.currencyFormat = new Intl.NumberFormat('pt-br', {
       style: 'currency',
       currency: 'BRL'
     })
   } 
+
+  calculateFinalPrice(customer, carCategory, numberOfDays) {
+    const { age } = customer;
+    const price = carCategory.price;
+    const { then: tax } = this.taxesBasedOnAge
+      .find((tax) => age >= tax.from && age <= tax.to);
+
+    const finalPrice = ((tax * price) * (numberOfDays));
+
+    const formattedPrice = this.currencyFormat.format(finalPrice);
+
+    return formattedPrice;
+  }
 
   getRandomPositionFromArray(list) {
     const listLength = list.length;
