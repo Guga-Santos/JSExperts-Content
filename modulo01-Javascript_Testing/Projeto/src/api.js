b655d7cac126746fa195ebae07ff7b6e517e737e
GugaSantos <guga.santos.guga@gmail.com>
2023-03-17 07:47:20 -0300

    feat: create get cars and get categories routes

--- modulo01-Javascript_Testing/Projeto/src/api.js
+++ modulo01-Javascript_Testing/Projeto/src/api.js
@@ -1,5 +1,12 @@
 const http = require('http');
+const { readFile } = require('fs/promises');
+const { join } = require('path');
 
+const categoriesDatabase = join(__dirname, './../database', 'carCategories.json');
+const carsDatabase = join(__dirname, './../database', 'cars.json');
+
+
+const BaseRepository = require('./repository/base/baseRepository');
 {/*
 Rotas a serem construidas:
 - Get Cars
@@ -7,12 +14,22 @@ Rotas a serem construidas:
 - Post Rent
 */}
 const routes = {
-  '/categories:get': (request, response) => {
-    response.write('Categoria');
+  '/:get': (request, response) => {
+    response.write('Página Inicial');
     return response.end();
   },
-  '/cars:get': (request, response) => {
-    response.write('Carros')
+  '/categories:get': async (request, response) => {
+    const data = JSON.parse(await readFile(categoriesDatabase, "utf8"))
+
+    response.writeHead(200)
+    response.write(JSON.stringify(data))
+    return response.end();
+  },
+  '/cars:get': async (request, response) => {
+    const data = JSON.parse(await readFile(carsDatabase, "utf8"))
+
+    response.writeHead(200)
+    response.write(JSON.stringify(data))
     return response.end();
   },
   '/totalprice:get': (request, response) => {
@@ -30,8 +47,8 @@ const routes = {
 }
 
 function handler(request, response) {
-  const { url, method } = request;
-  const routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`
+  const { url, method, params } = request;
+  let routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`
   const chosen = routes[routeKey] || routes.default;
   return chosen(request, response);
 }
